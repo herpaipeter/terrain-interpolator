1f0e74a58f8dc452c4c1a1298d6eeae1de574c7a
Herpai Péter <herpaip@datapress.hu>
2022-11-16 21:23:14 +0100

    Add array size check

--- src/main/java/hu/herpaipeter/TerrainInterpolator.java
+++ src/main/java/hu/herpaipeter/TerrainInterpolator.java
@@ -2,5 +2,18 @@ package hu.herpaipeter;
 
 public class TerrainInterpolator {
     public void interpolate(double[][] values, int size) {
+        if (1 < size && !isPowerOfTwo(size - 1))
+            throw new BadSize();
+    }
+
+    public boolean isPowerOfTwo(int num) {
+        if (num < 2)
+            return false;
+        while (num % 2 == 0)
+            num /= 2;
+        return num == 1;
+    }
+
+    public static class BadSize extends RuntimeException {
     }
 }
--- src/test/java/hu/herpaipeter/TerrainInterpolatorTest.java
+++ src/test/java/hu/herpaipeter/TerrainInterpolatorTest.java
@@ -5,6 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.text.IsEmptyString.emptyString;
 import static org.junit.Assert.assertThat;
 
@@ -25,6 +26,22 @@ public class TerrainInterpolatorTest {
         public void terminalCondition_sizeOne() throws Exception {
             interpolator.interpolate(dummy, 1);
             assertThat(actions, emptyString());
+        }
+
+        @Test(expected = TerrainInterpolator.BadSize.class)
+        public void sizeMustBePowerOfTwoPlus1() throws Exception {
+            interpolator.interpolate(dummy, 2);
+        }
+
+        @Test
+        public void Check_isPowerOfTwo() throws Exception {
+            assertThat(interpolator.isPowerOfTwo(2), is(true));
+            assertThat(interpolator.isPowerOfTwo(4), is(true));
+            assertThat(interpolator.isPowerOfTwo(8), is(true));
+
+            assertThat(interpolator.isPowerOfTwo(1), is(false));
+            assertThat(interpolator.isPowerOfTwo(7), is(false));
+            assertThat(interpolator.isPowerOfTwo(18), is(false));
         }
     }
 }
